9d7cfc03550a8c856cc2dd4f46dccaf813cdce7d
David Calvo <dcalvo2@jhu.edu>
2020-09-11 22:57:45 -0400

    in blackboard

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ const creds = require("./creds");
   if (page.url() === "https://blackboard.jhu.edu/webapps/login/") {
     await page.click("#loginBox-JHU > h2 > a:nth-child(5)");
     console.log(page.url());
-    await page.click("#i0116");
-    await page.type(creds.username);
+    await page.waitForSelector("#i0116");
+    await page.type("#i0116", creds.username);
     await page.click("#idSIButton9");
-    await page.click("#i0118");
-    await page.type(creds.password);
+    await page.waitFor(2000);
+    await page.type("#i0118", creds.password);
     await page.click("#idSIButton9");
+    await page.waitFor(2000);
   }
   await page.screenshot({ path: "example.png" });
 
