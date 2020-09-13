d54d60914bb2634946b3b6fa97b721675775404b
marc-helou <marcerino15@gmail.com>
2020-09-12 22:56:10 -0400

    implemented JS calendar population

--- Bootstrap/js/calendar.js
+++ Bootstrap/js/calendar.js
@@ -1,45 +1,114 @@
+// fetches the current date
 var today = new Date();
 var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
 console.log(date);
+
+// An array of each active column in the calendar
 let cols = document.querySelectorAll(".calCol");
 
+// Bolds the current day
 for (var j = 0; j < cols.length; j++) {
-    if (cols[j].querySelector("p").innerHTML == ("" + today.getDate())) {
-        cols[j].querySelector("p").id = "today";
+    if (cols[j].querySelector("div").innerHTML == ("" + today.getDate())) {
+        cols[j].querySelector("div").id = "today";
         console.log(cols[j]);
         break;
     }
 }
 
+// Refresh function; populates calendar
 document.getElementById("refresh").addEventListener("click", async function () {
 
     // begin loading here; perhaps grey out or just have cool loading thing from NATo project
 
     let bbRequest = await fetch("http://localhost:3000/api/blackboard");
     let gsRequest = await fetch("http://localhost:3000/api/gradescope");
-    let gmRequest = await fetch("http://localhost:3000/api/groupme");
 
     bb = JSON.parse(bbRequest);
     gs = JSON.parse(gsRequest);
-    gm = JSON.parse(gmRequest);
-    
+
     // Blackboard calendar fill
     for (var i = 0; i < bb.length; i++) {
-        if(today.getMonth() == bb[i].dueDate.month) {
-            let div = cols[i];
-            let task = document.createElement("calTaskCard");
+
+        if (today.getMonth() == bb[i].dueDate.month) {
+
+            // Instantiating all HTML elements
+
+            let assignment = bb[i].assignment;
+            let time = bb[i].dueDate;
+            let div = cols[bb[i].dueDate.date].getElementsByClassName(".calTaskWrapper")[0];
+            let task = document.createElement("div");
+            let taskContent = document.createElement("div");
+            let taskTime = document.createElement("div");
+            let taskTitle = document.createElement("div");
+
+            // Setting all classes for stylization
+
+            task.className = "calTaskCard";
+            taskContent.className = "calTaskContent";
+            taskTime.className = "taskTime";
+            taskTitle.className = "taskTitle";
+            taskTitle.innerHTML = assignment;
+
+            // Converting time to string
+            if (time.hour == 12) {
+                taskTime.innerHTML = "" + 12 + time.minute + "PM";
+            }
+            if (time.hour < 12) {
+                taskTime.innerHTML = "" + time.hour + time.minute + "AM";
+            }
+            else {
+                taskTime.innerHTML = "" + (time.hour - 12) + time.minute + "PM";
+            }
+
+            // Appending new elements to calendar div
+            taskContent.appendChild(taskTime);
+            taskContent.appendChild(taskTitle);
+            task.appendChild(taskContent);
+            div.appendChild(Task);
+
         }
     }
 
+    // Gradescope calendar fill
+    for (var i = 0; i < gs.length; i++) {
 
-    // adding new divs to calendars
-    let oldDiv = document.getElementsByClassName("msgF")[0];
-    let a = document.createElement('a');
-    var linkText;
-    linkText = document.createTextNode("English");
-    a.appendChild(linkText);
-    a.title = "en_Link";
-    a.href = `${result.href_en}`;
-    oldDiv.appendChild(a);
+        if (today.getMonth() == gs[i].dueDate.month) {
 
+            // Instantiating all HTML elements
+
+            let assignment = gs[i].assignment;
+            let time = gs[i].dueDate;
+            let div = cols[gs[i].dueDate.date].getElementsByClassName(".calTaskWrapper")[0];
+            let task = document.createElement("div");
+            let taskContent = document.createElement("div");
+            let taskTime = document.createElement("div");
+            let taskTitle = document.createElement("div");
+
+            // Setting all classes for stylization
+
+            task.className = "calTaskCard";
+            taskContent.className = "calTaskContent";
+            taskTime.className = "taskTime";
+            taskTitle.className = "taskTitle";
+            taskTitle.innerHTML = assignment;
+
+            // Converting time to string
+            if (time.hour == 12) {
+                taskTime.innerHTML = "" + 12 + time.minute + "PM";
+            }
+            if (time.hour < 12) {
+                taskTime.innerHTML = "" + time.hour + time.minute + "AM";
+            }
+            else {
+                taskTime.innerHTML = "" + (time.hour - 12) + time.minute + "PM";
+            }
+
+            // Appending new elements to calendar div
+            taskContent.appendChild(taskTime);
+            taskContent.appendChild(taskTitle);
+            task.appendChild(taskContent);
+            div.appendChild(Task);
+
+        }
+    }
 });
