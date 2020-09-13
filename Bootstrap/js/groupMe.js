7c618eb6ceda1c45cd14c7c0fad7f2416e8c8830
David Calvo <dcalvo2@jhu.edu>
2020-09-13 08:21:02 -0400

    add log in button toggle

--- Bootstrap/js/groupMe.js
+++ Bootstrap/js/groupMe.js
@@ -1,35 +1,37 @@
 var messageJSON;
 
-async function populateMessages(){
-
-  var messages = document.querySelectorAll('.message');
+async function populateMessages() {
+  var messages = document.querySelectorAll(".message");
 
   var messageStringData = await fetch("http://localhost:3000/api/groupme");
+  try {
+    messageJSON = await messageStringData.json();
+    messages.forEach((message) => {
+      message.style.visibility = "visible";
+    });
+    document.querySelector(".loginButton").style.display = "none";
+    // set .message visibility property to visible
+  } catch {
+    document.querySelector(".loginButton").style.display = "block";
+  }
   var messageArray = [];
-  messageJSON = await messageStringData.json();
   console.log(messageJSON);
-  for(var i = 0; i < 6; i++){
-
+  for (var i = 0; i < 6; i++) {
     var messageInfo = messageJSON[i];
     var groupName = messageInfo.groupName;
     var senderName = messageInfo.lastMessagePreview.nickname;
     var messageText = messageInfo.lastMessagePreview.text;
     var profPic = messageInfo.lastMessagePreview.image_url;
 
-    messages[i].querySelector('.groupchatTitle').innerHTML = groupName;
-    if(profPic == "" || profPic == null){
-
-      messages[i].querySelector('.prof').src = "img/groupme2.png";
-
+    messages[i].querySelector(".groupchatTitle").innerHTML = groupName;
+    if (profPic == "" || profPic == null) {
+      messages[i].querySelector(".prof").src = "img/groupme2.png";
+    } else {
+      messages[i].querySelector(".prof").src = profPic;
     }
-    else{
-      messages[i].querySelector('.prof').src = profPic;
-    }
-    messages[i].querySelector('.senderName').innerHTML = senderName;
-    messages[i].querySelector('.text').innerHTML = messageText;
-
+    messages[i].querySelector(".senderName").innerHTML = senderName;
+    messages[i].querySelector(".text").innerHTML = messageText;
   }
-
 }
 
 populateMessages();
